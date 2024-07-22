6b47a9f0b7ea1c583ba194381ac6ecf5f3791e73
congbang-le <lecongbang314@gmail.com>
2024-07-22 17:00:07 +0700

    feat(live): add prod tf files

---
+++ prod/prod.tf
@@ -0,0 +1,11 @@
+module "eks-vpc" {
+  source          = "../module/eks-vpc"
+  region          = "ap-southeast-1"
+  vpc_cidrs       = "10.0.0.0/16"
+  subnet_cidrs    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  cluster_name    = "EKS-Cluster-Demo"
+  node_group_name = "EKS-Node-Group-Demo"
+  desired_nodes   = 1
+  max_nodes       = 2
+  min_nodes       = 1
+}
