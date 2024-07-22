2cc258af05d4d8fd419289e4277127ac7adba696
congbang-le <lecongbang314@gmail.com>
2024-07-22 16:59:47 +0700

    fix(module): update eks vpc module

---
+++ module/eks-vpc/iam-role.tf
@@ -0,0 +1,58 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name               = "EKS-Cluster-Role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_role.name
+}
+
+resource "aws_iam_role" "ng_role" {
+  name = "EKS-Node-Group-Role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.ng_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.ng_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.ng_role.name
+}
---
+++ module/eks-vpc/vpc.tf
@@ -0,0 +1,62 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = var.common_tags
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidrs)
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = element(var.subnet_cidrs, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index % length(data.aws_availability_zones.available.names))
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name                     = "subnet-${count.index}"
+      "kubernetes.io/role/elb" = 1
+    }
+  )
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = var.common_tags
+}
+
+resource "aws_eip" "nat_ip" {
+  domain   = "vpc"
+
+  tags = var.common_tags
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_ip.id
+  subnet_id     = aws_subnet.subnets[0].id
+
+  tags = var.common_tags
+}
+
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = var.common_tags
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.subnets)
+
+  subnet_id      = element(aws_subnet.subnets.*.id, count.index)
+  route_table_id = aws_route_table.rtb.id
+}
