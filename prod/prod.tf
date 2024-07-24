babbb9c606d9c3c900342e02038069a3d1e9cc1c
congbang-le <lecongbang314@gmail.com>
2024-07-24 16:05:15 +0700

    chore: rename module

--- prod/prod.tf
+++ prod/prod.tf
@@ -1,5 +1,5 @@
 module "eks-vpc" {
-  source          = "../module/eks-vpc"
+  source          = "../module/eks-ec2"
   region          = "ap-southeast-1"
   vpc_cidrs       = "10.0.0.0/16"
   subnet_cidrs    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
